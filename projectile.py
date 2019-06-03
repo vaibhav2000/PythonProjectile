62c8839d2677c1e8b6d465df0cc1920e695874c8
Vaibhav Gajbhiye <32796125+vaibhav2000@users.noreply.github.com>
2019-06-03 13:07:02 +0530

    Add files via upload

---
+++ projectile.py
@@ -0,0 +1,44 @@
+import sys,pygame,math
+
+def runTheGame():
+    pygame.init()
+    clock=pygame.time.Clock()
+
+    len=500
+    wid=300
+
+    speed= [2,1]
+
+    screen = pygame.display.set_mode((len,wid))
+    ball = pygame.image.load("ball.png")
+    ballrect = ball.get_rect()
+    
+ 
+    sign=1
+    while True:
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+       
+        ballrect= ballrect.move(speed)
+        speed[1]= sign*(math.sqrt(ballrect.bottom/5)) 
+
+        if ballrect.left<0 or ballrect.right>len:
+            speed[0]= -speed[0]
+ 
+        if ballrect.bottom>wid:         
+            sign=-1
+
+        if  ballrect.top<0:
+            sign=1
+
+        
+        screen.fill((255,255,255))
+        screen.blit(ball,ballrect)
+        pygame.display.flip() 
+
+
+runTheGame()                
